1eb7edffbb662912c05127a09f06d2e5745b9e5c
sjahl <sjahl@broadinstitute.org>
2021-07-22 10:34:45 -0400

    maybe this is what substr wants

--- gcs-egress-monitoring/variables.tf
+++ gcs-egress-monitoring/variables.tf
@@ -17,7 +17,7 @@ variable "sent_bytes_alerting_duration" {
   type        = string
   description = "The number of seconds to evaluate an alert threshold on. As a string, 60 seconds would be represented as '60s'"
   validation {
-    condition     = substr(var.sent_bytes_alerting_duration, -2, -1) == "s"
+    condition     = substr(var.sent_bytes_alerting_duration, -1, -1) == "s"
     error_message = "The time format must be a string, with the number of seconds, ending in the letter 's'."
   }
 }
@@ -26,7 +26,7 @@ variable "sent_bytes_alerting_alignment_period" {
   type        = string
   description = "The number of seconds to use for the alignment period when evaluating an alert condition. As a string, 60 seconds would be represented as '60s'"
   validation {
-    condition     = substr(var.sent_bytes_alerting_alignment_period, -2, -1) == "s"
+    condition     = substr(var.sent_bytes_alerting_alignment_period, -1, -1) == "s"
     error_message = "The time format must be a string, with the number of seconds, ending in the letter 's'."
   }
 }
