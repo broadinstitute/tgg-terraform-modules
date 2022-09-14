28a996676dda11cfbfd68788d392e9a5e7270f1b
sjahl <636687+sjahl@users.noreply.github.com>
2022-09-14 13:51:42 -0400

    first pass at a module for granting users scoped dataproc access

---
+++ dataproc-service-acct/variables.tf
@@ -0,0 +1,19 @@
+variable "user_principal" {
+  type        = string
+  description = "The email address of the user we'd like to grant access to"
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "The name that we should use as the username portion of the service account email address"
+}
+
+variable "service_account_display_name" {
+  type        = string
+  description = "The display name of the service account that we are going to create"
+}
+
+variable "dataproc_bucket_prefix" {
+  type        = string
+  description = "The prefix that we should use for the names of the stage and temp buckets for Dataproc."
+}
