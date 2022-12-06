533efd0a9d04fa646eb8469de5acc6a114c524db
sjahl <636687+sjahl@users.noreply.github.com>
2022-12-06 11:21:11 -0500

    fix variable reference in node_config

--- gnomad-browser-infra/main.tf
+++ gnomad-browser-infra/main.tf
@@ -186,7 +186,7 @@ resource "google_container_node_pool" "node_pool" {
 
   node_config {
     machine_type    = each.value.pool_machine_type
-    preemptible     = each.value.pool_machine_type
+    preemptible     = each.value.pool_preemptible
     service_account = google_service_account.gke_cluster_sa.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
