694594ddd7fa995268c29acc72beb1e641245b17
sjahl <636687+sjahl@users.noreply.github.com>
2023-02-06 11:15:32 -0500

    specify the project ID for bucket creation

--- gnomad-google-project/storage.tf
+++ gnomad-google-project/storage.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "general_use_bucket" {
   count         = var.create_default_buckets ? 1 : 0
+  project       = google_project.current_project.project_id
   name          = "${var.project_id}-storage"
   location      = var.default_resource_region
   storage_class = "STANDARD"
@@ -11,6 +12,7 @@ resource "google_storage_bucket" "general_use_bucket" {
 # TODO: add a -tmp-4day bucket for auto-deletion
 resource "google_storage_bucket" "tmp_4day_bucket" {
   count         = var.create_default_buckets ? 1 : 0
+  project       = google_project.current_project.project_id
   name          = "${var.project_id}-tmp-4day"
   location      = var.default_resource_region
   storage_class = "STANDARD"
