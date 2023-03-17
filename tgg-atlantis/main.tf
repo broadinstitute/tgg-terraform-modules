f2a1f83dc614a802f10eef7b9b6b5ef6399f681f
sjahl <636687+sjahl@users.noreply.github.com>
2023-03-17 16:05:54 -0400

    Add a module for deploying atlantis

---
+++ tgg-atlantis/main.tf
@@ -0,0 +1,25 @@
+# include GKE cluster
+
+module "atlantis-gke" {
+  source           = "github.com/broadinstitute/tgg-terraform-modules//private-gke-cluster?ref=private-gke-cluster-v0.0.1"
+  gke_cluster_name = "tgg-atlantis"
+  project_name     = "tgg-automation"
+  resource_labels = {
+    "terraform"  = "true"
+    "deployment" = "atlantis"
+    "component"  = "gke"
+  }
+  vpc_network_name = var.vpc_network_name
+  vpc_subnet_name  = var.vpc_subnet_name
+}
+
+# atlantis service account
+resource "google_service_account" "atlantis_runner" {
+  account_id   = "tgg-atlantis-runner"
+  display_name = "TGG Atlantis Runner"
+}
+
+# static IP, tls cert etc
+resource "google_compute_global_address" "atlantis_ip" {
+  name = "TGG Atlantis"
+}
