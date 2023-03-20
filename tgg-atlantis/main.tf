c9d9c3d55a57d969cfc342b7aea0b925a2bb570e
sjahl <636687+sjahl@users.noreply.github.com>
2023-03-20 11:09:16 -0400

    fix ref to submodule

--- tgg-atlantis/main.tf
+++ tgg-atlantis/main.tf
@@ -1,7 +1,7 @@
 # include GKE cluster
 
 module "atlantis-gke" {
-  source           = "github.com/broadinstitute/tgg-terraform-modules//private-gke-cluster?ref=a9eea7c9f2e5059d478a52f4d658909365943373"
+  source           = "github.com/broadinstitute/tgg-terraform-modules//private-gke-cluster?ref=c54aabc767cb87d49c7862f633940314d952445f"
   gke_cluster_name = "tgg-atlantis"
   project_name     = "tgg-automation"
   resource_labels = {
