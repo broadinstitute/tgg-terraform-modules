d5186aedf46160f10f6bd723ea18fe11a9b19693
sjahl <636687+sjahl@users.noreply.github.com>
2023-04-07 15:48:18 -0400

    add subnet name output back in

--- gnomad-vpc/outputs.tf
+++ gnomad-vpc/outputs.tf
@@ -2,3 +2,7 @@ output "gnomad_vpc_network_name" {
   value       = module.gnomad-vpc.vpc_network_name
   description = "The name of the VPC network created by this module"
 }
+
+output "gnomad_vpc_subnet_names" {
+  value = module.gnomad-vpc.vpc_subnet_names
+}
