47a0255cc07f71b04af1e05fd30fb1192343c6d2
sjahl <636687+sjahl@users.noreply.github.com>
2023-04-07 15:43:08 -0400

    remove old outputs

--- gnomad-vpc/outputs.tf
+++ gnomad-vpc/outputs.tf
@@ -1,13 +1,4 @@
-output "gnomad_subnet_secondary_ranges" {
-  value       = [for range in google_compute_subnetwork.gnomad_subnet.secondary_ip_range : range.range_name]
-  description = "The secondary ranges associated with the gnomad subnet"
-}
-
 output "gnomad_vpc_network_name" {
   value       = google_compute_network.network.name
   description = "The name of the VPC network created by this module"
-}
-
-output "gnomad_vpc_network_gke_subnet_name" {
-  value = google_compute_subnetwork.gnomad_subnet.name
 }
