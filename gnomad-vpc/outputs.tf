19561d11c6556f2e007c8d48c7dedb0875912d23
sjahl <636687+sjahl@users.noreply.github.com>
2023-04-07 15:45:24 -0400

    fix outputs and accesses again

--- gnomad-vpc/outputs.tf
+++ gnomad-vpc/outputs.tf
@@ -1,4 +1,4 @@
 output "gnomad_vpc_network_name" {
-  value       = google_compute_network.network.name
+  value       = module.gnomad-vpc.vpc_network_name
   description = "The name of the VPC network created by this module"
 }
