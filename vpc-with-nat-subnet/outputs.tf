2d08d36863ae3a57d4f92631d3063b65fae245bb
sjahl <636687+sjahl@users.noreply.github.com>
2023-04-07 14:59:07 -0400

    handle multiple subnet name outputs

--- vpc-with-nat-subnet/outputs.tf
+++ vpc-with-nat-subnet/outputs.tf
@@ -2,6 +2,6 @@ output "vpc_network_name" {
   value = google_compute_network.network.name
 }
 
-output "vpc_subnet_name" {
-  value = google_compute_subnetwork.subnet.name
+output "vpc_subnet_names" {
+  value = [for subnet in google_compute_subnetwork.subnet : subnet.name]
 }
