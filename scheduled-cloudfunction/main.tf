4fc465cfd164bda62fe66e4dd3f1f88e470382d9
sjahl <636687+sjahl@users.noreply.github.com>
2023-08-25 10:45:32 -0400

    reference conditional/counted resources by ID

--- scheduled-cloudfunction/main.tf
+++ scheduled-cloudfunction/main.tf
@@ -25,7 +25,7 @@ resource "google_service_account" "scheduled_function_service_account" {
 
 resource "google_service_account_iam_member" "cloudbuild_impersonate" {
   count              = var.manage_service_accounts ? 1 : 0
-  service_account_id = google_service_account.scheduled_function_service_account.name
+  service_account_id = google_service_account.scheduled_function_service_account[0].name
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${var.cloudbuild_service_account_email}"
 }
@@ -34,14 +34,14 @@ resource "google_project_iam_member" "service_account_project_permissions" {
   for_each = toset(var.service_account_roles)
   project  = var.project_id
   role     = each.value
-  member   = google_service_account.scheduled_function_service_account.member
+  member   = google_service_account.scheduled_function_service_account[0].member
 }
 
 resource "google_secret_manager_secret_iam_member" "function_secret_access" {
   for_each  = toset(var.required_gcp_secrets)
   secret_id = each.value
   role      = "roles/secretmanager.secretAccessor"
-  member    = google_service_account.scheduled_function_service_account.member
+  member    = google_service_account.scheduled_function_service_account[0].member
 }
 
 resource "google_service_account" "deployment_service_account" {
@@ -55,14 +55,14 @@ resource "google_project_iam_member" "deployment_service_account_project_permiss
   count   = var.manage_service_accounts ? 1 : 0
   project = var.project_id
   role    = "roles/cloudfunctions.admin"
-  member  = google_service_account.deployment_service_account.member
+  member  = google_service_account.deployment_service_account[0].member
 }
 
 resource "google_service_account_iam_member" "deployer_impersonate" {
   count              = var.manage_service_accounts ? 1 : 0
-  service_account_id = google_service_account.scheduled_function_service_account.name
+  service_account_id = google_service_account.scheduled_function_service_account[0].name
   role               = "roles/iam.serviceAccountUser"
-  member             = google_service_account.deployment_service_account.member
+  member             = google_service_account.deployment_service_account[0].member
 }
 
 module "gh_oidc_wif" {
@@ -82,7 +82,7 @@ module "gh_oidc_wif" {
   attribute_condition = var.workload_identity_attr_condition
   sa_mapping = {
     "${var.scheduled_function_name}-deployer" = {
-      sa_name   = google_service_account.deployment_service_account.id
+      sa_name   = google_service_account.deployment_service_account[0].id
       attribute = var.workload_identity_attr
     }
   }
@@ -94,11 +94,11 @@ output "scheduled_function_trigger_topic" {
 }
 
 output "service_account_member" {
-  value = var.manage_service_accounts ? google_service_account.scheduled_function_service_account.member : null
+  value = var.manage_service_accounts ? google_service_account.scheduled_function_service_account[0].member : null
 }
 
 output "deployment_service_account_member" {
-  value = var.manage_service_accounts ? google_service_account.deployment_service_account.member : null
+  value = var.manage_service_accounts ? google_service_account.deployment_service_account[0].member : null
 }
 
 output "scheduled_function_name" {
