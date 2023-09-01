f6edbd360f2640da03254bc6cdfe1256892c68dd
sjahl <636687+sjahl@users.noreply.github.com>
2023-09-01 14:25:49 -0400

    byo service accounts and remove a bunch of gross logic

--- scheduled-cloudfunction/main.tf
+++ scheduled-cloudfunction/main.tf
@@ -16,16 +16,16 @@ resource "google_cloud_scheduler_job" "cloud_scheduler_schedule" {
   }
 }
 
-resource "google_service_account" "scheduled_function_service_account" {
-  count        = var.manage_service_accounts ? 1 : 0
-  account_id   = var.scheduled_function_name
-  project      = var.project_id
-  display_name = "Service account for scheduled function: ${var.scheduled_function_name}"
+data "google_service_account" "runtime_service_account" {
+  account_id = var.runtime_service_account_email
+}
+
+data "google_service_account" "deployment_service_account" {
+  account_id = var.deployment_service_account_email
 }
 
 resource "google_service_account_iam_member" "cloudbuild_impersonate" {
-  count              = var.manage_service_accounts ? 1 : 0
-  service_account_id = google_service_account.scheduled_function_service_account[0].name
+  service_account_id = data.google_service_account.runtime_service_account.name
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${var.cloudbuild_service_account_email}"
 }
@@ -34,35 +34,26 @@ resource "google_project_iam_member" "service_account_project_permissions" {
   for_each = toset(var.service_account_roles)
   project  = var.project_id
   role     = each.value
-  member   = google_service_account.scheduled_function_service_account[0].member
+  member   = data.google_service_account.runtime_service_account.member
 }
 
 resource "google_secret_manager_secret_iam_member" "function_secret_access" {
   for_each  = toset(var.required_gcp_secrets)
   secret_id = each.value
   role      = "roles/secretmanager.secretAccessor"
-  member    = google_service_account.scheduled_function_service_account[0].member
-}
-
-resource "google_service_account" "deployment_service_account" {
-  count        = var.manage_service_accounts ? 1 : 0
-  account_id   = "${var.scheduled_function_name}-deployer"
-  project      = var.project_id
-  display_name = "Service account for scheduled function deployment: ${var.scheduled_function_name}"
+  member    = data.google_service_account.runtime_service_account.member
 }
 
 resource "google_project_iam_member" "deployment_service_account_project_permissions" {
-  count   = var.manage_service_accounts ? 1 : 0
   project = var.project_id
   role    = "roles/cloudfunctions.admin"
-  member  = google_service_account.deployment_service_account[0].member
+  member  = data.google_service_account.deployment_service_account.member
 }
 
 resource "google_service_account_iam_member" "deployer_impersonate" {
-  count              = var.manage_service_accounts ? 1 : 0
-  service_account_id = google_service_account.scheduled_function_service_account[0].name
+  service_account_id = data.google_service_account.runtime_service_account.name
   role               = "roles/iam.serviceAccountUser"
-  member             = google_service_account.deployment_service_account[0].member
+  member             = data.google_service_account.deployment_service_account.member
 }
 
 module "gh_oidc_wif" {
@@ -82,7 +73,7 @@ module "gh_oidc_wif" {
   attribute_condition = var.workload_identity_attr_condition
   sa_mapping = {
     "${var.scheduled_function_name}-deployer" = {
-      sa_name   = google_service_account.deployment_service_account[0].id
+      sa_name   = data.google_service_account.deployment_service_account.id
       attribute = var.workload_identity_attr
     }
   }
@@ -91,14 +82,6 @@ module "gh_oidc_wif" {
 
 output "scheduled_function_trigger_topic" {
   value = google_pubsub_topic.scheduled_function_trigger_topic.name
-}
-
-output "service_account_member" {
-  value = var.manage_service_accounts ? google_service_account.scheduled_function_service_account[0].member : null
-}
-
-output "deployment_service_account_member" {
-  value = var.manage_service_accounts ? google_service_account.deployment_service_account[0].member : null
 }
 
 output "scheduled_function_name" {
