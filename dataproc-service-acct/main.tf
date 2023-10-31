279121ab4a5ea3d8b5718038ded12432099248ba
sjahl <636687+sjahl@users.noreply.github.com>
2023-10-31 09:23:31 -0400

    grant legacy GCS perms on dataproc buckets

--- dataproc-service-acct/main.tf
+++ dataproc-service-acct/main.tf
@@ -54,9 +54,21 @@ resource "google_storage_bucket_iam_member" "grant_user_dataproc_stage_write" {
   member = "user:${var.user_principal}"
 }
 
+resource "google_storage_bucket_iam_member" "grant_user_dataproc_stage_legacy_write" {
+  bucket = google_storage_bucket.user_dataproc_stage.name
+  role   = "roles/storage.legacyBucketWriter"
+  member = "user:${var.user_principal}"
+}
+
 resource "google_storage_bucket_iam_member" "grant_user_dataproc_temp_write" {
   bucket = google_storage_bucket.user_dataproc_temp.name
   role   = "roles/storage.objectAdmin"
+  member = "user:${var.user_principal}"
+}
+
+resource "google_storage_bucket_iam_member" "grant_user_dataproc_temp_legacy_write" {
+  bucket = google_storage_bucket.user_dataproc_temp.name
+  role   = "roles/storage.legacyBucketWriter"
   member = "user:${var.user_principal}"
 }
 
@@ -66,8 +78,20 @@ resource "google_storage_bucket_iam_member" "dataproc_sa_stage_write" {
   member = "serviceAccount:${var.service_account_email}"
 }
 
+resource "google_storage_bucket_iam_member" "dataproc_sa_stage_legacy_write" {
+  bucket = google_storage_bucket.user_dataproc_stage.name
+  role   = "roles/storage.legacyBucketWriter"
+  member = "serviceAccount:${var.service_account_email}"
+}
+
 resource "google_storage_bucket_iam_member" "dataproc_sa_temp_write" {
   bucket = google_storage_bucket.user_dataproc_temp.name
   role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${var.service_account_email}"
+}
+
+resource "google_storage_bucket_iam_member" "dataproc_sa_temp_legacy_write" {
+  bucket = google_storage_bucket.user_dataproc_temp.name
+  role   = "roles/storage.legacyBucketWriter"
   member = "serviceAccount:${var.service_account_email}"
 }
